eb3b529617d8516d3b93a5bf028c7dbb978a36dc
Thiago <martinsmthiago@gmail.com>
2025-09-15 23:36:14 -0300

    Home page render / route

--- server.js
+++ server.js
@@ -1,18 +1,26 @@
-const express = require("express");
-const dotenv = require("dotenv");
-const cors = require("cors");
-const path = require("path");
-const methodOverride = require("method-override");
-const expressLayouts = require("express-ejs-layouts");
-
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const path = require('path');
+const methodOverride = require('method-override');
+const expressLayouts = require('express-ejs-layouts');
 
 const app = express();
 //Load env vars
 dotenv.config();
 //Connect to database
-connectDB();
+//connectDB();
 //Routes
 
+//View engine setup
+app.use(expressLayouts);
+app.set('view engine', 'ejs');
+app.set('layout', 'layouts/main');
+
+//Home route
+app.get('/', (req, res) => {
+  res.render('index.ejs');
+});
 
 //Server
 const PORT = process.env.PORT || 8080;
